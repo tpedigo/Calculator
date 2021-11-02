7c20887ad0754a533b97103ae20af2e6eb4082f4
Tara Pedigo <tkirkhus@gmail.com>
2021-11-02 19:04:55 -0400

    Add storeExpression, clearDisplay, and edit updateDisplay

--- script.js
+++ script.js
@@ -30,13 +30,68 @@ function operate(a, b, operator) {
 }
 
 function updateDisplay(e) {
-    if (display.textContent.length % 12 === 0) {
-        display.textContent += ("\n" + e.target.textContent);
-    } else {
+    if (display.textContent.length === 0 && operators.includes(e.target.textContent)) {
+        display.textContent = "ERROR: must start with number";
+        display.style.fontSize = "22px";
+        display.style.textAlign = "start";
+    
+    } else if (display.textContent.length < 12) {
         display.textContent += e.target.textContent;
+    } else {
+        display.style.fontSize = "24px";
+        display.style.textAlign = "start";
+        display.textContent = "ERROR: number too large";
+    }
+    for (i=0; i<display.textContent.length - 1; i++) {
+        if (operators.includes(display.textContent[i]) && operators.includes(display.textContent[i+1])) {
+            display.textContent = display.textContent.slice(0, -1);
+            display.textContent = "ERROR: two operators in a row";
+            display.style.fontSize = "20px";
+            display.style.textAlign = "start";
+        }
     }
 }
 
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function storeExpression(e) {
+    if (e.target.classList.contains("number") && !currentExpression.hasOperator) {
+        currentExpression.A += e.target.textContent;
+        // console.log("Hi");
+    } else if (e.target.classList.contains("operator")&& !currentExpression.hasOperator) {
+        currentExpression.operator = e.target.textContent;
+        currentExpression.hasOperator = true;
+        // console.log("Hey");
+
+    } else if (e.target.classList.contains("number") && currentExpression.hasOperator) {
+        currentExpression.B += e.target.textContent;
+        // console.log("Hello");
+    } else if (e.target.classList.contains("operator") && currentExpression.hasOperator) {
+
+    } else {
+        console.log("bye");
+    }
+    console.log(currentExpression);
+    return currentExpression;
+}
+
+function calculate(e) {
+
+}
+
+let currentExpression = {A: "", hasOperator: false, B: ""};
+// let currentExpression = {hasOperator: false};
+const operators = ["+", "-", "x", "%"];
 const display = document.querySelector(".display");
-const numbers = document.querySelectorAll(".number");
-numbers.forEach(number => number.addEventListener("click", updateDisplay));
+const numberButtons = document.querySelectorAll(".number");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
+const operatorButtons = document.querySelectorAll(".operator");
+clear.addEventListener("click", clearDisplay);
+numberButtons.forEach(number => number.addEventListener("click", updateDisplay));
+numberButtons.forEach(number => number.addEventListener("click", storeExpression));
+operatorButtons.forEach(operator => operator.addEventListener("click", updateDisplay));
+operatorButtons.forEach(operator => operator.addEventListener("click", storeExpression));
+equals.addEventListener("click", calculate);
