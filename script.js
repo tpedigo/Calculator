2a85bdd2ab1b3e27de2cd9afebe6b54789113ed8
Tara Pedigo <tkirkhus@gmail.com>
2021-11-03 13:04:02 -0400

    Clean up operate and formatting

--- script.js
+++ script.js
@@ -1,48 +1,101 @@
+let result;
+let currentExpression = {A: "", hasOperator: false, B: ""};
+const operators = ["+", "-", "x", "%"];
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+const clear = document.querySelector(".clear");
+const equals = document.querySelector(".equals");
+const operatorButtons = document.querySelectorAll(".operator");
+clear.addEventListener("click", clearDisplay);
+numberButtons.forEach(number => number.addEventListener("click", updateDisplay));
+// numberButtons.forEach(number => number.addEventListener("click", storeExpression));
+operatorButtons.forEach(operator => operator.addEventListener("click", updateDisplay));
+// operatorButtons.forEach(operator => operator.addEventListener("click", storeExpression));
+equals.addEventListener("click", equalsEvent);
+
+function equalsEvent() {
+    display.textContent = operate(+currentExpression.A, +currentExpression.B, currentExpression.operator);
+}
+
 function add(a, b) {
-    return a + b;
+    return (a+b) % 1 === 0 ? a+b : (a+b).toFixed(3);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return (a-b) % 1 === 0 ? a-b : (a-b).toFixed(3);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return (a*b) % 1 === 0 ? a*b : (a*b).toFixed(3);
 }
 
 function divide(a, b) {
-    return a / b;
+    return (a/b) % 1 === 0 ? a/b : (a/b).toFixed(3);
 }
 
-function updateDisplay(e) {
-    if (display.textContent == result) {
-        display.textContent = 0;
+function checkErrors() {
+    for (i=0; i<display.textContent.length-1; i++) {
+        if (operators.includes(display.textContent[i]) && operators.includes(display.textContent[i+1])) {
+            display.style.fontSize = "20px";
+            display.style.textAlign = "start";
+            display.textContent = "ERROR: two operators in a row";
+            return true;
+        } else {
+            return false;
+        }
     }
+}
+function updateDisplay(e) {
+    // if (display.textContent == result) {
+    //     display.textContent = 0;
+    // }
     if (display.textContent.length === 0 && operators.includes(e.target.textContent)) {
-        display.textContent = "ERROR: must start with number";
         display.style.fontSize = "22px";
         display.style.textAlign = "start";
-    
+        display.textContent = "ERROR: must start with number";
+        numberButtons.forEach(number => number.removeEventListener("click", updateDisplay));
+        operatorButtons.forEach(operator => operator.removeEventListener("click", updateDisplay));
+        clear.removeEventListener("click", updateDisplay);
+        equals.removeEventListener("click", equalsEvent); 
+        currentExpression = {A: "", hasOperator: false, B: ""};
     } else if (display.textContent.length < 12) {
         display.textContent += e.target.textContent;
+        for (i=0; i<display.textContent.length - 1; i++) {
+            if (operators.includes(display.textContent[i]) && operators.includes(display.textContent[i+1])) {          
+                display.style.fontSize = "20px";
+                display.style.textAlign = "start";
+                display.textContent = "ERROR: two operators in a row";
+                numberButtons.forEach(number => number.removeEventListener("click", updateDisplay));
+                operatorButtons.forEach(operator => operator.removeEventListener("click", updateDisplay));
+                clear.removeEventListener("click", updateDisplay);
+                equals.removeEventListener("click", equalsEvent); 
+                currentExpression = {A: "", hasOperator: false, B: ""};
+            }
+        }
+        storeExpression(e);
     } else {
         display.style.fontSize = "24px";
         display.style.textAlign = "start";
         display.textContent = "ERROR: number too large";
-    }
-    for (i=0; i<display.textContent.length - 1; i++) {
-        if (operators.includes(display.textContent[i]) && operators.includes(display.textContent[i+1])) {
-            display.textContent = display.textContent.slice(0, -1);
-            display.textContent = "ERROR: two operators in a row";
-            display.style.fontSize = "20px";
-            display.style.textAlign = "start";
-        }
+        numberButtons.forEach(number => number.removeEventListener("click", updateDisplay));
+        operatorButtons.forEach(operator => operator.removeEventListener("click", updateDisplay));
+        clear.removeEventListener("click", updateDisplay);
+        equals.removeEventListener("click", equalsEvent);
+        currentExpression = {A: "", hasOperator: false, B: ""};
     }
 }
 
 function clearDisplay() {
     display.textContent = "";
+    display.style.fontSize = "32px";
+    display.style.textAlign = "end";
     currentExpression = {A: "", hasOperator: false, B: ""};
+    clear.addEventListener("click", clearDisplay);
+    numberButtons.forEach(number => number.addEventListener("click", updateDisplay));
+    // numberButtons.forEach(number => number.addEventListener("click", storeExpression));
+    operatorButtons.forEach(operator => operator.addEventListener("click", updateDisplay));
+    // operatorButtons.forEach(operator => operator.addEventListener("click", storeExpression));
+    equals.addEventListener("click", equalsEvent);
 }
 
 function storeExpression(e) {
@@ -64,28 +117,16 @@ function storeExpression(e) {
 
 function operate(a, b, operator) {
     if (operator==="+") {
+        currentExpression = {A: add(a, b), hasOperator: false, B: ""};
         return add(a, b);
     } else if (operator==="-") {
+        currentExpression = {A: subtract(a, b), hasOperator: false, B: ""};
         return subtract(a, b);
     } else if (operator==="x") {
+        currentExpression = {A: multiply(a, b), hasOperator: false, B: ""};
         return multiply(a, b);
     } else {
+        currentExpression = {A: divide(a, b), hasOperator: false, B: ""};
         return divide(a, b);
     }
 }
-
-let result;
-let currentExpression = {A: "", hasOperator: false, B: ""};
-const operators = ["+", "-", "x", "%"];
-const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number");
-const clear = document.querySelector(".clear");
-const equals = document.querySelector(".equals");
-const operatorButtons = document.querySelectorAll(".operator");
-clear.addEventListener("click", clearDisplay);
-numberButtons.forEach(number => number.addEventListener("click", updateDisplay));
-numberButtons.forEach(number => number.addEventListener("click", storeExpression));
-operatorButtons.forEach(operator => operator.addEventListener("click", updateDisplay));
-operatorButtons.forEach(operator => operator.addEventListener("click", storeExpression));
-equals.addEventListener("click", () =>
-    display.textContent = operate(+currentExpression.A, +currentExpression.B, currentExpression.operator));
